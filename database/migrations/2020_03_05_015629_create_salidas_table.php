385af3ad21442632d0c5bb98186d9db1c88d9cf4
keysiwin12 <keysiwin25@gmail.com>
2020-03-04 21:31:22 -0500

    migrations

---
+++ database/migrations/2020_03_05_015629_create_salidas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalidasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salidas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('fecha');
+            $table->date('hora');
+            $table->double('precio');
+            $table->unsignedBigInteger('empresa_id');
+            $table->foreign('empresa_id')->references('id')->on('empresas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salidas');
+    }
+}
